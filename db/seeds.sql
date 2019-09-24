6b4a3fe7263331942be552da9cdba53743140345
LuisT <ltaborda0615@gmail.com>
2019-09-23 21:01:30 -0400

    added all initial files

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ("Bacon Burger");
+INSERT INTO burgers (burger_name , devoured) VALUES ("Everything Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Minetta burger", true);
+INSERT INTO burgers (burger_name) VALUES ("Umami Burger");
+INSERT INTO burgers (burger_name) VALUES ("All American Burger");
