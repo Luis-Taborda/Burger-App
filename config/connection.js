f1d9ebc52b97794c5241bc7f747a818a3cd5423e
LuisT <ltaborda0615@gmail.com>
2019-09-28 20:39:24 -0400

    more bug fixes

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,25 @@
-var mysql = require("mysql");
+const mysql = require("mysql")
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
+var connection;
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
 
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "L0615zt32a",
+    database: "burgers_db"
+  });
+}
+
+
+
+// Make connection.
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
@@ -16,4 +28,5 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
+// Export connection for our ORM to use.
 module.exports = connection;
