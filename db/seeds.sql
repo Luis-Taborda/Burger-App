2bec4faa1e90898de9094bf12ec27415c2f568f0
LuisT <ltaborda0615@gmail.com>
2019-12-03 18:54:29 -0500

    heroku push

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,3 +3,4 @@ INSERT INTO burgers (burger_name , devoured) VALUES ("Everything Burger", true);
 INSERT INTO burgers (burger_name, devoured) VALUES ("Minetta burger", true);
 INSERT INTO burgers (burger_name) VALUES ("Umami Burger");
 INSERT INTO burgers (burger_name) VALUES ("All American Burger");
+INSERT INTO burgers (burger_name) VALUES ("dirty burger");
